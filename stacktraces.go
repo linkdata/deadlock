16819d2dbb9401abf7389aa593bcfd0cf07826de
Johan Lindh <johan@linkdata.se>
2023-03-24 12:29:33 +0100

    simplify stacktraces

--- stacktraces.go
+++ stacktraces.go
@@ -1,15 +1,11 @@
 package deadlock
 
 import (
-	"bytes"
 	"fmt"
 	"io"
-	"os"
-	"os/user"
-	"path/filepath"
 	"runtime"
 	"strings"
-	"sync"
+	"sync/atomic"
 )
 
 func callers(skip int) []uintptr {
@@ -18,89 +14,37 @@ func callers(skip int) []uintptr {
 }
 
 func printStack(w io.Writer, stack []uintptr) {
-	home := os.Getenv("HOME")
-	usr, err := user.Current()
-	if err == nil {
-		home = usr.HomeDir
-	}
-	cwd, _ := os.Getwd()
-
-	for i, pc := range stack {
-		f := runtime.FuncForPC(pc)
-		name := f.Name()
-		pkg := ""
-		if pos := strings.LastIndex(name, "/"); pos >= 0 {
-			name = name[pos+1:]
-		}
-		if pos := strings.Index(name, "."); pos >= 0 {
-			pkg = name[:pos]
-			name = name[pos+1:]
-		}
-		file, line := f.FileLine(pc)
-		if (pkg == "runtime" && name == "goexit") || (pkg == "testing" && name == "tRunner") {
-			fmt.Fprintln(w)
-			return
-		}
-		tail := ""
-		if i == 0 {
-			tail = " <<<<<" // Make the line performing a lock prominent.
-		}
-		// Shorten the file name.
-		clean := file
-		if cwd != "" {
-			cl, err := filepath.Rel(cwd, file)
-			if err == nil {
-				clean = cl
+	for _, pc := range stack {
+		if f := runtime.FuncForPC(pc); f != nil {
+			var pkg string
+			name := f.Name()
+			if pos := strings.LastIndexByte(name, '/'); pos >= 0 {
+				name = name[pos+1:]
 			}
-		}
-		if home != "" {
-			s2 := strings.Replace(file, home, "~", 1)
-			if len(clean) > len(s2) {
-				clean = s2
+			if pos := strings.IndexByte(name, '.'); pos >= 0 {
+				pkg = name[:pos]
+				name = name[pos+1:]
+				if (pkg == "runtime" && name == "goexit") || (pkg == "testing" && name == "tRunner") {
+					break
+				}
 			}
+			file, line := f.FileLine(pc)
+			fmt.Fprintf(w, "\t%s:%d %s.%s\n", file, line-1, pkg, name)
 		}
-		fmt.Fprintf(w, "%s:%d %s.%s %s%s\n", clean, line-1, pkg, name, code(file, line), tail)
 	}
 	fmt.Fprintln(w)
 }
 
-var fileSources struct {
-	sync.Mutex
-	lines map[string][][]byte
-}
-
-// Reads souce file lines from disk if not cached already.
-func getSourceLines(file string) [][]byte {
-	fileSources.Lock()
-	defer fileSources.Unlock()
-	if fileSources.lines == nil {
-		fileSources.lines = map[string][][]byte{}
-	}
-	if lines, ok := fileSources.lines[file]; ok {
-		return lines
-	}
-	text, _ := os.ReadFile(file)
-	fileSources.lines[file] = bytes.Split(text, []byte{'\n'})
-	return fileSources.lines[file]
-}
-
-func code(file string, line int) string {
-	lines := getSourceLines(file)
-	line -= 2
-	if line >= len(lines) || line < 0 {
-		return "???"
-	}
-	return "{ " + string(bytes.TrimSpace(lines[line])) + " }"
-}
+var stackBufSize = int64(1024)
 
 // Stacktraces for all goroutines.
 func stacks() []byte {
-	buf := make([]byte, 1024*16)
 	for {
-		n := runtime.Stack(buf, true)
-		if n < len(buf) {
+		bufSize := atomic.LoadInt64(&stackBufSize)
+		buf := make([]byte, bufSize)
+		if n := runtime.Stack(buf, true); n < len(buf) {
 			return buf[:n]
 		}
-		buf = make([]byte, 2*len(buf))
+		atomic.StoreInt64(&stackBufSize, bufSize*2)
 	}
 }
